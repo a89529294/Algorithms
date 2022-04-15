cf7e526d5b9b830509383e95adeb7e29042e1c94
Albert Chang <a89529294@hotmail.com>
2022-04-15 14:28:39 +0800

    add binary search

---
+++ BinarySearch.js
@@ -0,0 +1,17 @@
+const list = [1, 3, 5, 7, 9];
+
+const randomNum = Math.ceil(Math.random() * 100);
+
+function binarySearch(list, target) {
+  let leftIdx = 0;
+  let rightIdx = list.length - 1;
+  while (leftIdx <= rightIdx) {
+    const midIdx = parseInt((leftIdx + rightIdx) / 2);
+    if (list[midIdx] === target) return midIdx;
+    else if (list[midIdx] < target) leftIdx = midIdx + 1;
+    else rightIdx = midIdx - 1;
+  }
+  return null;
+}
+
+console.log(binarySearch(list, 11));
