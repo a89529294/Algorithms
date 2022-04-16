6d7074001e05b5c2cbc4e8e50d7776957d924902
Albert Chang <a89529294@hotmail.com>
2022-04-16 17:04:12 +0800

    add another example of recursion

--- Recursions.js
+++ Recursions.js
@@ -1,4 +1,9 @@
-// Find biggest suqare that fills up the whole rectangle (Euclidean Algorithm)
+// Divide & Conquer:
+// 1. Figure out a simple case as the base case.
+// 2. Figure out how to reduce your problem and get to the base case.
+
+// Example 1. Find biggest suqare that fills up the whole rectangle (Euclidean Algorithm)
+// In this example the base case is where one side is a multiple of the other side
 
 function findBiggestSquare(width, height) {
   if (width % height === 0 || height % width === 0) {
@@ -15,3 +20,16 @@ function findBiggestSquare(width, height) {
 }
 
 console.log(findBiggestSquare(1680, 640));
+
+// Example 2. Summation of an array
+// base case is when the array is one single item
+
+// assuming arr is an array of numbers
+function sum(arr) {
+  if (arr.length === 0) return 0;
+  else {
+    return arr[0] + sum(arr.slice(1));
+  }
+}
+
+console.log(sum([1, 2, 3, 101]));
