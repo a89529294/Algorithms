757045c3e18d87c9cbcaded115234cc8fd7deca9
Albert Chang <a89529294@hotmail.com>
2022-04-18 15:37:23 +0800

    add hashtable

---
+++ HashTable.js
@@ -0,0 +1,7 @@
+// A hash function is a function that takes in a string and returns a number
+// There are two requirements:
+// 1. It needs to be consistent, for the same string the function should return the same number.
+// 2. Every string should map to an unique number.
+
+// You can make a hash table by combining a hash function with an array.
+// Hash tables have really fast search, insert, and delete. O(1) time
